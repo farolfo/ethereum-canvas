d8568b13ff7dd8b411d634581147f58f1d1de2d8
Franco Arolfo <francoarolfo@hotmail.com>
2017-10-14 19:28:31 -0300

    Fixed tabs

--- contracts/EthereumCanvas.sol
+++ contracts/EthereumCanvas.sol
@@ -9,9 +9,9 @@ contract EthereumCanvas {
      * Event of a pixel's purchase.
      */
     event Purchase (
-        address owner,	// owner of pixel
-        uint price, 	// price payed for the pixel
-        string color, 	// color in HEX or any string acceptable by CSS3 as a colour
+        address owner,  // owner of pixel
+        uint price,     // price payed for the pixel
+        string color,   // color in HEX or any string acceptable by CSS3 as a colour
         uint x,         // the x coordinate
         uint y          // the y coordinate
     );
@@ -19,55 +19,55 @@ contract EthereumCanvas {
     /**
      * A pixel of the window.
      */
-	struct Pixel {
-		address owner;	// owner of pixel
-		uint price;		// price payed for the pixel
-		string color; 	// color in HEX or any string acceptable by CSS3 as a colour
-	}
+    struct Pixel {
+        address owner;  // owner of pixel
+		uint price;     // price payed for the pixel
+		string color;   // color in HEX or any string acceptable by CSS3 as a colour
+    }
 
-	/**
-	 * The window is a map that fits "width -> (height -> pixel)".
-	 */
-	mapping (uint => mapping (uint => Pixel)) private window;
+    /**
+     * The window is a map that fits "width -> (height -> pixel)".
+     */
+    mapping (uint => mapping (uint => Pixel)) private window;
 
     /**
      * The maximum value of the window's width.
      */
-	uint maxWidth = 1000;
+    uint maxWidth = 1000;
 
     /**
      * The maximum value of the window's height.
      */
-	uint maxHeight = 1000;
+    uint maxHeight = 1000;
 
     /**
      * Function used to buy the pixel (x,y) and set the given color.
      */
-	function buyPixel(uint x, uint y, string color) payable {
-	    if (isOutOfBound(x,y) || msg.value <= window[x][y].price) {
-			revert();
-		}
+    function buyPixel(uint x, uint y, string color) payable {
+        if (isOutOfBound(x,y) || msg.value <= window[x][y].price) {
+            revert();
+        }
 
         window[x][y] = Pixel({owner: msg.sender, price: msg.value, color: color});
         Purchase(msg.sender, msg.value, color, x, y);
-	}
+    }
 
     /**
      * Function used to check the current status of the pixel at (x,y).
      */
-	function checkPixel(uint x, uint y) public constant returns(address, uint, string) {
-	    if (isOutOfBound(x,y)) {
-    	    revert();
+    function checkPixel(uint x, uint y) public constant returns(address, uint, string) {
+        if (isOutOfBound(x,y)) {
+            revert();
         }
 
-		var pixel = window[x][y];
-		return (pixel.owner, pixel.price, pixel.color);
-	}
+        var pixel = window[x][y];
+        return (pixel.owner, pixel.price, pixel.color);
+    }
 
 	/**
 	 * Function that checks whether the parameters are out of bound respect to the current window.
 	 */
-	function isOutOfBound(uint x, uint y) public constant returns(bool) {
+    function isOutOfBound(uint x, uint y) public constant returns(bool) {
         return x < 0 || y < 0 || x >= maxWidth || y >= maxHeight;
     }
 }
